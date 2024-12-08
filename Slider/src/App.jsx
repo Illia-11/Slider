6b42bfd016ddf9ca7b2e64d22084bd8e778ff93f
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-08 09:24:23 +0200

    Initial commit

---
+++ Slider/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SliderComponent from "./components/SliderComponent";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ Slider/src/components/SliderComponent/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class SliderComponent extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default SliderComponent;
